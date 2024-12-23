19417b2db3b12431576141cc312664a16fc0ee26
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-12-23 19:08:04 -0300

    Password methods to User  Model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,26 +1,40 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, 'The userName is required'],
-        trim: true, 
-        minlength: [3, 'The userName must be at least 3 characters long'],
-    },
-    email: {
-        type: String,
-        required: [true, 'The email is required'],
-        unique: true,
-        match: [/.+@.+\..+/, 'Please enter a valid email address'],
-    },
-    password: {
-        type: String,
-        required: [true, 'The password is required'],
-        minlength: [6, 'The password must be at least 6 characters long'],
-    },
+  username: {
+    type: String,
+    required: [true, 'The username is required'],
+    trim: true,
+    minlength: [3, 'The username must be at least 3 characters long'],
+  },
+  email: {
+    type: String,
+    required: [true, 'The email is required'],
+    unique: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'The password is required'],
+    minlength: [6, 'The password must be at least 6 characters long'],
+  },
 }, {
-    timestamps: true, 
+  timestamps: true,
 });
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
