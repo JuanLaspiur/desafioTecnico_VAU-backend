283d857253c6dc4e0da72b685dcfbd7d95a6eea7
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-12-23 17:14:29 -0300

    Get users width JWT token

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 
 router.get(`/id/:id`, userController.getUserById);
-router.get(`/`, userController.getAllUsers);
+router.get(`/`, authMiddleware, userController.getAllUsers);
 router.put(`/id/:id`, userController.updateUserByid);
 router.put(`/`, authMiddleware, userController.updateUser);
 router.delete(`/id/:id`, userController.deleteUserById)
